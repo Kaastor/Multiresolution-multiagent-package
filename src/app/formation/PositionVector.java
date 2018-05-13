5a199496e698f465dff8a65b00bd532096f3305a
Przemek Czuba <przemek@generalaudittool.com>
2018-05-13 22:35:28 +0200

    Implemented PositionVector class

--- src/app/formation/PositionVector.java
+++ src/app/formation/PositionVector.java
@@ -1,5 +1,42 @@
 package app.formation;
 
 
+import app.Agent;
+import javafx.geometry.Point2D;
+
 public class PositionVector {
+
+    private Agent sourceAgent;
+    private Point2D vector;
+    private Agent targetAgent;
+
+    public PositionVector(Agent sourceAgent, Point2D vector, Agent targetAgent) {
+        this.sourceAgent = sourceAgent;
+        this.vector = vector;
+        this.targetAgent = targetAgent;
+    }
+
+    public Agent getSourceAgent() {
+        return sourceAgent;
+    }
+
+    public void setSourceAgent(Agent sourceAgent) {
+        this.sourceAgent = sourceAgent;
+    }
+
+    public Point2D getVector() {
+        return vector;
+    }
+
+    public void setVector(Point2D vector) {
+        this.vector = vector;
+    }
+
+    public Agent getTargetAgent() {
+        return targetAgent;
+    }
+
+    public void setTargetAgent(Agent targetAgent) {
+        this.targetAgent = targetAgent;
+    }
 }
