fd0e55bf7d8711bc919ea749d74fec25d3084ae2
Przemek Czuba <przemek@generalaudittool.com>
2018-05-15 21:35:35 +0200

    Skeleton for JavaFX window

--- src/app/App.java
+++ src/app/App.java
@@ -2,18 +2,64 @@ package app;
 
 import dissim.simspace.core.SimControlException;
 import dissim.simspace.core.SimModel;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.concurrent.Task;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
-public class App {
+public class App extends Application {
+
+    public static final double sceneWidth = 1024;
+    public static final double sceneHeight = 768;
+
+    private Visualization visualization = new Visualization(sceneWidth, sceneHeight);
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        visualization.init();
+
+        BorderPane root = new BorderPane();
+        root.setCenter(visualization);
+
+        Task task = new Task<Void>() {
+            @Override public Void call() throws SimControlException{
+                startSimulation();
+                return null;
+            }
+        };
+        Thread thread = new Thread(task);
+        thread.setDaemon(true);
+        thread.start();
+
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                try {
+                    Thread.sleep(50);
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        Scene scene = new Scene(root, sceneWidth, sceneHeight, Color.GRAY);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void startSimulation() throws SimControlException{
+        SimModel model = SimModel.getInstance();
+        System.out.println("Start simulation");
+        model.startSimulation();
+    }
 
     public static void main(String[] args) {
-        try {
-            //SimModel(SimManager) - obiekt zarządzający symulacją
-            SimModel model = SimModel.getInstance();
-            System.out.println("Start simulation");
-            model.startSimulation();
-
-        } catch (SimControlException var5) {
-            var5.printStackTrace();
-        }
+        launch(args);
     }
 }
--- src/app/Visualization.java
+++ src/app/Visualization.java
@@ -1,5 +1,27 @@
 package app;
 
 
-public class Visualization {
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+
+class Visualization extends Pane {
+
+    Circle circle = new Circle();
+
+    Visualization(double sceneWidth, double sceneHeight) {
+        setWidth(sceneWidth);
+        setHeight(sceneHeight);
+    }
+
+    void init() throws Exception {
+
+        circle.setStroke(Color.BLACK);
+        circle.setFill(Color.BLACK);
+        circle.setCenterX(100);
+        circle.setCenterY(100);
+        circle.setRadius(10d);
+        this.getChildren().addAll(circle);
+    }
 }
